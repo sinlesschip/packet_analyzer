c63a712df49cfeeeaee0f66133fbe73257be03ac
cody <cody.poloski@gmail.com>
2025-06-23 02:33:25 -0500

    output formatting

--- analyzer.c
+++ analyzer.c
@@ -28,7 +28,7 @@ void packet_handler(u_char *args,
         strncpy(src_ip, inet_ntoa(ip_header->ip_src), INET_ADDRSTRLEN);
         strncpy(dst_ip, inet_ntoa(ip_header->ip_dst), INET_ADDRSTRLEN);
 
-        printf("Source IP: %15s ---> Destination IP: %s\t Protocol: %s\n",
+        printf("Source IP: %15s\t--->\tDestination IP: %15s\t(%s)\n",
                src_ip, dst_ip, get_protocol_name(ip_header->ip_p));
     }
 
